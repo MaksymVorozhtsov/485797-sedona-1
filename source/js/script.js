2f97a6781630e1acc8fe04e2b9e9a2198358a434
MaksymVorozhtsov <maxim.vorojtsov@gmail.com>
2018-10-17 22:43:39 +0200

    let's scripts started

---
+++ source/js/script.js
@@ -0,0 +1,11 @@
+var openButton = document.querySelector('.header__button--open');
+var closeButton = document.querySelector('.header__button--close');
+var menuList = document.querySelector('.main-nav');
+
+openButton.classList.remove('no-js');
+closeButton.classList.remove('no-js');
+menuList.classList.remove('no-js');
+
+closeButton.classList.add('header__button--hide');
+
+
