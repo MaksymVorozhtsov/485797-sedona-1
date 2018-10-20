480e82a16dfe157b61134782176308fbcd41f02b
MaksymVorozhtsov <maxim.vorojtsov@gmail.com>
2018-10-20 13:24:28 +0200

    optimization + js

--- source/js/script.js
+++ source/js/script.js
@@ -12,4 +12,9 @@ openButton.addEventListener("click", function() {
   menuList.classList.add("main-nav--opened");
   siteLogo.classList.add("site-logo--menuopened");
   closeButton.classList.remove("header__button--hide");
+  closeButton.addEventListener("click", function() {
+    menuList.classList.remove("main-nav--opened");
+    closeButton.classList.add("header__button--hide");
+    openButton.classList.remove("header__button--hide");
+  });
 });
